bed9cc8add4cedeaf017fa1da6c2a5998709fb6b
Manuel Neri <a01329872@itesm.mx>
2020-11-13 15:59:28 -0600

    pipes 1

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -10,6 +10,8 @@ export class ProductComponent implements OnInit,OnDestroy{
   @Input() product:Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
+  today = new Date();
+
   constructor() {
     console.log('1. constructor');
   }
