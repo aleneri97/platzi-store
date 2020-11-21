6be9cbb6d67d13b8690a72be12e91aa59911d82a
Manuel Neri <a01329872@itesm.mx>
2020-11-21 17:12:27 -0600

    Shared/Core Modules

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,15 +3,17 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     BannerComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     HomeRoutingModule,
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 
