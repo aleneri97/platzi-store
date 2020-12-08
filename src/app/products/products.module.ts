ddf2aacc2055e2093cbbf77b420e97639b0fb4a9
Manuel Neri <a01329872@itesm.mx>
2020-12-08 15:57:08 -0600

    Instalación de Angular y primer componente

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
+import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductComponent } from './components/product/product.component';
@@ -9,6 +10,6 @@ import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
   declarations: [ProductComponent, ProductsComponent, ProductDetailComponent],
-  imports: [ProductsRoutingModule, CommonModule, SharedModule],
+  imports: [ProductsRoutingModule, CommonModule, SharedModule, MaterialModule],
 })
 export class ProductsModule {}
