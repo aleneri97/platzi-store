6fa39f559a4656f8d19ab5d3ff4e2c4bb4d92d3d
Manuel Neri <manuel.neri@pgr.gob.mx>
2021-04-23 13:58:05 -0500

    Async Pipe

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CartService } from 'src/app/core/services/cart/cart.service';
 
 @Component({
@@ -8,11 +10,12 @@ import { CartService } from 'src/app/core/services/cart/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$: Observable<number>;
+
   constructor(private cartService: CartService) {
-    this.cartService.cart$.subscribe(cart => {
-      this.total = cart.length;
-    })
+    this.total$ = this.cartService.cart$.pipe(
+      map(products => products.length)
+    );
   }
 
   ngOnInit() {
