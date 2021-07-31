a4adbc2e58e8cf951ffeedffcf61446a523f06f9
Alejandro Neri <ale_neri@yahoo.com>
2021-07-30 19:09:14 -0500

    Actualiza partes faltantes del proyecto

--- src/app/products/components/product-detail/product-detail.component.ts
+++ src/app/products/components/product-detail/product-detail.component.ts
@@ -1,11 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Product } from '../../../product.model';
 
 import { ProductsService } from '@core/services/products/products.service';
-import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Product } from 'src/app/product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -16,12 +14,50 @@ export class ProductDetailComponent implements OnInit {
 
   product$: Observable<Product>;
 
-  constructor(private route: ActivatedRoute, private productsService: ProductsService, private http: HttpClient) { }
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit() {
     this.product$ = this.route.params
     .pipe(
-      switchMap((params: Params) => this.productsService.getProduct(params.id))
+      switchMap((params: Params) => {
+        return this.productsService.getProduct(params.id);
+      })
     );
+  }
+
+  createProduct() {
+    const newProduct: Product = {
+      id: '222',
+      title: 'nuevo desde angular',
+      image: 'assets/images/banner-1.jpg',
+      price: 3000,
+      description: 'nuevo producto'
+    };
+    this.productsService.createProduct(newProduct)
+    .subscribe(product => {
+      console.log(product);
+    });
+  }
+
+  updateProduct() {
+    const updateProduct: Partial<Product> = {
+      price: 555555,
+      description: 'edicion titulo'
+    };
+    this.productsService.updateProduct('2', updateProduct)
+    .subscribe(product => {
+      console.log(product);
+    });
+  }
+
+  deleteProduct() {
+    this.productsService.deleteProduct('222')
+    .subscribe(rta => {
+      console.log(rta);
+    });
+  }
   }
 }
