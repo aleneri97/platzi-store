ea69d405b0bd03d84615d5e1ab8e7b8b97c81936
Alejandro Neri <ale_neri@yahoo.com>
2021-08-02 18:54:11 -0500

    Pruebas unitarias

--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,12 +1,83 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Product } from 'src/app/product.model';
+import { environment } from 'src/environments/environment';
 
 import { ProductsService } from './products.service';
 
-xdescribe('ProductsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('ProductsService', () => {
+  let httpClient: HttpClient;
+  let httpTestingService: HttpTestingController;
+  let service: ProductsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingService = TestBed.get(HttpTestingController);
+    service = TestBed.get(ProductsService);
+  });
 
   it('should be created', () => {
-    const service: ProductsService = TestBed.get(ProductsService);
     expect(service).toBeTruthy();
+  });
+
+});
+
+
+describe('Test for getAllProducts', () => {
+  let httpClient: HttpClient;
+  let httpTestingService: HttpTestingController;
+  let service: ProductsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingService = TestBed.get(HttpTestingController);
+    service = TestBed.get(ProductsService);
+  });
+
+  it('should return an array of products', () => {
+    // arrange
+    const expectedProducts: Product[] = [{
+      id: '1',
+      title: 'Product 1',
+      price: 10,
+      description: 'Description 1',
+      image: 'http://placehold.it/200x200',
+    }, {
+      id: '2',
+      title: 'Product 2',
+      price: 20,
+      description: 'Description 2',
+      image: 'http://placehold.it/200x200',
+    }];
+
+    let responseError: any;
+    let responseData: Product[];
+
+    // act
+    service.getAllProducts()
+      .subscribe( data => {
+          responseData = data;
+        }, error => {
+          responseError = error;
+        }
+      );
+    const req = httpTestingService.expectOne(`${environment.url_api}/products`);
+    req.flush(expectedProducts);
+
+    // assert
+    expect(responseData).toEqual(expectedProducts);
+    expect(responseError).toBeUndefined();
+    expect(req.request.method).toEqual('GET');
+    expect(responseData.length).toEqual(2);
+
   });
 });
