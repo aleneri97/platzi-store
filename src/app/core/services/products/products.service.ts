18efc9882a49f106e266c5a19cf57fcd6d384998
Alejandro Neri <ale_neri@yahoo.com>
2021-08-02 13:21:23 -0500

    Accediendo a archivos con Http

--- src/app/core/services/products/products.service.ts
+++ src/app/core/services/products/products.service.ts
@@ -46,6 +46,10 @@ export class ProductsService {
         map((res: any) => res.results as User[]));
   }
 
+  getFile() {
+    return this.http.get('assets/files/test.txt', { responseType: 'text' });
+  }
+
   private handleError(error: HttpErrorResponse ) {
     console.error(error);
     Sentry.captureException(error);
